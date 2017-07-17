695e28fc7f735ec3415ee73941ea98ff4177e38d
COMI <admin@wincomi.com>
2017-07-17 23:21:51 +0900

    fix #2

--- replace_word.addon.php
+++ replace_word.addon.php
@@ -11,23 +11,28 @@ if(!defined("__XE__")) {
 
 if($called_position == 'before_module_init' && ($this->act === 'procBoardInsertDocument' || $this->act === 'procBoardInsertComment'))
 {
+	$title = Context::get('title');
 	$content = Context::get('content');
 
 	if($addon_info->type1 == 'Y')
 	{
+		$title = preg_replace("/됬/", "됐", $title);
 		$content = preg_replace("/됬/", "됐", $content);
 	}
 	
 	if($addon_info->type2 == 'Y')
 	{
+		$title = preg_replace("/되요/", "돼요", $title);
 		$content = preg_replace("/되요/", "돼요", $content);
 	}
 	
 	if($addon_info->type3 == 'Y')
 	{
+		$title = preg_replace("/되서/", "돼서", $title);
 		$content = preg_replace("/되서/", "돼서", $content);
 	}	
 
+	Context::set('title', $title);
 	Context::set('content', $content);
 }
 
